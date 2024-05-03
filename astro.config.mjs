7bf446e44ae5097c9d50e84b1bb9823cf50ec2d7
Breno Lasserre <lasserrebreno@gmail.com>
2024-05-02 23:05:18 -0300

    Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-
-import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind(), react()],
 });
