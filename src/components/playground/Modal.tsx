c06181883fccf5b3f7eb75367aa9d319f365163e
Breno Lasserre <lasserrebreno@gmail.com>
2024-05-10 18:09:34 -0300

    Cambios Modal

--- src/components/playground/Modal.tsx
+++ src/components/playground/Modal.tsx
@@ -31,16 +31,14 @@ export default function SharedLayout() {
 
   return (
     <div className='relative flex h-full w-full flex-col items-center justify-center'>
-      <MotionConfig
-        transition={{ type: 'spring', bounce: 0.25, duration: 0.8 }}
-      >
+      <MotionConfig transition={{ type: 'spring', bounce: 0.2, duration: 0.6 }}>
         {modalOpen ? (
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.2 }}
-            className='pointer-events-none absolute inset-0 top-0 h-full w-full bg-black/10'
+            className='pointer-events-none absolute inset-0 top-0 h-full w-full bg-red-700/20'
           />
         ) : null}
 
@@ -59,16 +57,16 @@ export default function SharedLayout() {
                         key={buttonState}
                         layoutId='content'
                         initial={{
-                          y: 50,
+                          y: 20,
                           opacity: 0,
-                          filter: 'blur(4px)',
+                          filter: 'blur(2px)',
                         }}
                         animate={{
                           y: 0,
                           opacity: 1,
                           filter: 'blur(0px)',
                         }}
-                        exit={{ y: 50, opacity: 0, filter: 'blur(4px)' }}
+                        exit={{ y: 20, opacity: 0, filter: 'blur(2px)' }}
                         className='mb-8'
                       >
                         <span className='my-4 flex items-center justify-center'>
@@ -83,11 +81,18 @@ export default function SharedLayout() {
                               layoutId='icon'
                               transition={{
                                 yoyo: Infinity,
-                                ease: 'easeInOut',
-                                duration: 0.5,
+                                duration: 0.35,
                               }}
-                              initial={{ pathLength: 0 }}
-                              animate={{ pathLength: 1 }}
+                              initial={{
+                                pathLength: 0,
+                                y: 20,
+                                filter: 'blur(4px)',
+                              }}
+                              animate={{
+                                pathLength: 1,
+                                y: 0,
+                                filter: 'blur(0px)',
+                              }}
                               strokeLinecap='round'
                               strokeLinejoin='round'
                               d='M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z'
@@ -97,23 +102,35 @@ export default function SharedLayout() {
 
                         <motion.h2
                           layoutId='header'
-                          exit={{ opacity: 0 }}
+                          layout='position'
                           animate={{ opacity: 1 }}
                           initial={{ opacity: 0 }}
                           className='my-2 text-2xl font-semibold text-white'
                         >
-                          Are you sure?
+                          <motion.span
+                            className='flex items-center justify-center gap-1'
+                            layoutId='header-1'
+                            layout='position'
+                          >
+                            Are you sure?
+                          </motion.span>
                         </motion.h2>
 
                         <motion.p
+                          layout='position'
                           layoutId='header-text'
-                          exit={{ opacity: 0 }}
                           animate={{ opacity: 1 }}
                           initial={{ opacity: 0 }}
                           className='text-zinc-400'
                         >
-                          This action is <u>not reversible</u>. You will delete
-                          your project and all of the information you've stored.
+                          <motion.span
+                            className='flex items-center justify-center gap-1'
+                            layoutId='paragraph-1'
+                            layout='position'
+                          >
+                            This action is not reversible. You will delete your
+                            project and all of the information you've stored.
+                          </motion.span>
                         </motion.p>
                       </motion.div>
                     ) : (
@@ -142,21 +159,34 @@ export default function SharedLayout() {
                           </span>
                           <motion.h2
                             layoutId='header'
+                            layout='position'
                             exit={{ y: 20, filter: 'blur(2px)' }}
                             animate={{ y: 0, filter: 'blur(0px)' }}
                             className='my-2 text-2xl font-semibold text-white'
                           >
-                            Deleting Project
+                            <motion.span
+                              className='flex items-center justify-center gap-1'
+                              layoutId='header-1'
+                              layout='position'
+                            >
+                              Deleting Project
+                            </motion.span>
                           </motion.h2>
                           <motion.p
+                            layout
                             layoutId='header-text'
                             exit={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
-                            initial={{ opacity: 0 }}
                             className='text-zinc-400'
                           >
-                            Tip: You can create new projects anytime at{' '}
-                            <u>/projects</u>.
+                            <motion.span
+                              className='flex items-center justify-center gap-1'
+                              layoutId='paragraph-1'
+                              layout='position'
+                            >
+                              Tip: You can create new projects anytime at
+                              /projects.
+                            </motion.span>
                           </motion.p>
                         </motion.div>
                       </AnimatePresence>
@@ -233,20 +263,26 @@ export default function SharedLayout() {
                           }, 3000);
                         }}
                       >
-                        <svg
-                          fill='none'
-                          viewBox='0 0 24 24'
-                          strokeWidth={2}
-                          stroke='currentColor'
-                          className='h-4 w-4'
+                        <motion.span
+                          className='flex items-center justify-center gap-1'
+                          layoutId='delete-button'
+                          layout='position'
                         >
-                          <path
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                            d='m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0'
-                          />
-                        </svg>
-                        {buttonContent[buttonState]}
+                          <svg
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            strokeWidth={2}
+                            stroke='currentColor'
+                            className='h-4 w-4'
+                          >
+                            <path
+                              strokeLinecap='round'
+                              strokeLinejoin='round'
+                              d='m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0'
+                            />
+                          </svg>
+                          {buttonContent[buttonState]}
+                        </motion.span>
                       </motion.button>
                     </div>
                   </div>
@@ -278,7 +314,9 @@ export default function SharedLayout() {
                   style={{ fontSize: 16, borderRadius: 10, width: '100%' }}
                   className=' bg-[#230D0E] px-20 py-2 text-[#DA3036]'
                 >
-                  {buttonContent[buttonState]}
+                  <motion.span layoutId='delete-button' layout='position'>
+                    {buttonContent[buttonState]}
+                  </motion.span>
                 </motion.button>
               </div>
             </motion.div>
