dfbad2d357682699aa1757cccfcfd8479a821657
Breno Lasserre <lasserrebreno@gmail.com>
2024-06-28 18:38:40 -0300

    ChatGPT Fix

--- src/components/playground/ChatGPT.tsx
+++ src/components/playground/ChatGPT.tsx
@@ -3,7 +3,14 @@
 import { useState } from 'react';
 import useMeasure from 'react-use-measure';
 
-import { motion, AnimatePresence, MotionConfig } from 'framer-motion';
+import {
+  motion,
+  AnimatePresence,
+  MotionConfig,
+  easeInOut,
+  easeIn,
+  easeOut,
+} from 'framer-motion';
 
 export default function SharedLayout() {
   const [open, setOpen] = useState<boolean>(false);
@@ -12,16 +19,16 @@ export default function SharedLayout() {
   return (
     <MotionConfig
       transition={{
-        duration: 0.4,
+        duration: 0.45,
         type: 'spring',
-        bounce: 0.2,
+        bounce: 0,
       }}
     >
       <div className='flex w-full items-center justify-center gap-2'>
         <motion.div className='flex items-center justify-between'>
           <motion.div className='w-fit'>
             {open ? (
-              <AnimatePresence>
+              <AnimatePresence initial={false}>
                 <motion.div
                   className='flex items-center justify-center gap-2'
                   initial='hidden'
@@ -29,54 +36,65 @@ export default function SharedLayout() {
                   exit='hidden'
                   style={{ height: '40px' }}
                 >
-                  <motion.img
-                    initial={{ opacity: 0, scale: 0 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    exit={{ opacity: 0, scale: 0 }}
-                    transition={{ delay: 0.4 }}
-                    src='/chat/clip.svg'
-                    alt='clip'
-                  />
-                  <motion.img
-                    initial={{ opacity: 0, x: -30, scale: 0 }}
-                    animate={{ opacity: 1, x: 0, scale: 1 }}
-                    exit={{ opacity: 0, x: -30, scale: 0 }}
-                    transition={{ delay: 0.2 }}
-                    src='/chat/folder.svg'
-                    alt='folder'
-                  />
-                  <motion.img
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    src='/chat/img.svg'
-                    alt='img'
-                    layoutId='enterIcon'
-                  />
+                  <motion.button
+                    whileTap={{ scale: 0.95 }}
+                    onClick={() => setOpen(!open)}
+                  >
+                    <motion.img
+                      initial={{ opacity: 0, scale: 0 }}
+                      animate={{ opacity: 1, scale: 1 }}
+                      exit={{ opacity: 0, scale: 0 }}
+                      transition={{ delay: 0.4 }}
+                      src='/chat/close.svg'
+                      alt='close'
+                    />
+                  </motion.button>
+                  <motion.button whileTap={{ scale: 0.95 }}>
+                    <motion.img
+                      initial={{ opacity: 0, x: -30, scale: 0 }}
+                      animate={{ opacity: 1, x: 0, scale: 1 }}
+                      exit={{ opacity: 0, x: -30, scale: 0 }}
+                      transition={{ delay: 0.2 }}
+                      src='/chat/clip.svg'
+                      alt='clip'
+                    />
+                  </motion.button>
+                  <motion.button whileTap={{ scale: 0.95 }}>
+                    <motion.img
+                      key='img'
+                      initial={{ opacity: 0.5, scale: 0, x: -60 }}
+                      animate={{ opacity: 1, scale: 1, x: 0 }}
+                      exit={{ opacity: 0, scale: 0, x: -60 }}
+                      src='/chat/img.svg'
+                      alt='img'
+                    />
+                  </motion.button>
                 </motion.div>
               </AnimatePresence>
             ) : (
-              <motion.button
-                style={{ height: '40px' }}
-                onClick={() => setOpen(!open)}
-                className='w-10 rounded-full bg-[#201F20] p-2'
-                layoutId='enterIcon'
-                initial={{ filter: 'blur(4px)', opacity: 0 }}
-                animate={{ filter: 'blur(0px)', opacity: 1 }}
-                exit={{ filter: 'blur(4px)', opacity: 0 }}
-              >
-                <motion.img src='/chat/add.svg' alt='add' />
-              </motion.button>
+              <AnimatePresence initial={false}>
+                <motion.button
+                  whileTap={{ scale: 0.95 }}
+                  style={{ height: '40px' }}
+                  onClick={() => setOpen(!open)}
+                  className='w-10 rounded-full bg-[#201F20] p-2'
+                  initial={{ opacity: 0, x: 60 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 60 }}
+                >
+                  <motion.img src='/chat/add.svg' alt='add' />
+                </motion.button>
+              </AnimatePresence>
             )}
           </motion.div>
         </motion.div>
 
         <motion.div
           className='relative ml-auto flex items-center  rounded-full border-2 border-[#201F20] text-[#787878]'
+          style={{ height: '40px' }}
           initial={{ width: '100%' }}
           animate={{
             width: open ? 'calc(100% - 100px)' : '100%',
-            height: '40px',
           }}
           ref={ref}
         >
@@ -89,11 +107,7 @@ export default function SharedLayout() {
             <div className='pointer-events-none absolute right-0 top-0 h-full w-24 rounded-full bg-gradient-to-r from-transparent to-[#08090A] to-60%'></div>
           </div>
           <span className='absolute right-2 top-1/2 -translate-y-1/2 transform'>
-            <img
-              src='/chat/send.svg'
-              alt='send'
-              onClick={() => setOpen(!open)}
-            />
+            <img src='/chat/send.svg' alt='send' />
           </span>
         </motion.div>
       </div>
