1f95ab24351ca550e8eed82986638eeb5bfac5c0
Breno Lasserre <lasserrebreno@gmail.com>
2024-06-29 15:01:14 -0300

    ChatGPT Fix animation

--- src/components/playground/ChatGPT.tsx
+++ src/components/playground/ChatGPT.tsx
@@ -10,7 +10,7 @@ export default function SharedLayout() {
   return (
     <MotionConfig
       transition={{
-        duration: 0.4,
+        duration: 0.6,
         type: 'spring',
         bounce: 0.3,
       }}
@@ -34,42 +34,63 @@ export default function SharedLayout() {
                     onClick={() => setOpen(!open)}
                   >
                     <motion.img
-                      initial={{ opacity: 0, scale: 0 }}
-                      animate={{ opacity: 1, scale: 1 }}
-                      exit={{ opacity: 0, scale: 0 }}
+                      initial={{ scale: 0 }}
+                      animate={{ scale: 1 }}
+                      exit={{ scale: 0 }}
                       transition={{
-                        duration: 0.4,
+                        duration: 0.6,
                         type: 'spring',
                         bounce: 0,
-                        delay: 0.3,
-                      }}
-                      src='/chat/close.svg'
-                      alt='close'
-                    />
-                  </motion.button>
-                  <motion.button whileTap={{ scale: 0.95 }}>
-                    <motion.img
-                      initial={{ opacity: 0, x: -20, scale: 0 }}
-                      animate={{ opacity: 1, x: 0, scale: 1 }}
-                      exit={{ opacity: 0, x: -20, scale: 0 }}
-                      transition={{
-                        duration: 0.4,
-                        type: 'spring',
-                        bounce: 0,
-                        delay: 0.15,
+                        delay: 0.4,
                       }}
                       src='/chat/clip.svg'
                       alt='clip'
                     />
                   </motion.button>
-                  <motion.button whileTap={{ scale: 0.95 }}>
+                  <motion.button
+                    whileTap={{ scale: 0.95 }}
+                    onClick={() => setOpen(!open)}
+                  >
+                    <motion.img
+                      initial={{ filter: 'blur(4px)', x: -30, scale: 0 }}
+                      animate={{ filter: 'blur(0px)', x: 0, scale: 1 }}
+                      exit={{ filter: 'blur(4px)', x: -30, scale: 0 }}
+                      transition={{
+                        duration: 0.6,
+                        type: 'spring',
+                        bounce: 0,
+                        delay: 0.2,
+                      }}
+                      src='/chat/folder.svg'
+                      alt='folder'
+                    />
+                  </motion.button>
+                  <motion.button
+                    whileTap={{ scale: 0.95 }}
+                    onClick={() => setOpen(!open)}
+                  >
                     <motion.img
                       key='img'
-                      initial={{ opacity: 0.5, scale: 0, x: -60 }}
-                      animate={{ opacity: 1, scale: 1, x: 0 }}
-                      exit={{ opacity: 0, scale: 0, x: -60 }}
+                      initial={{
+                        filter: 'blur(6px)',
+                        opacity: 0.5,
+                        scale: 0,
+                        x: -60,
+                      }}
+                      animate={{
+                        filter: 'blur(0px)',
+                        opacity: 1,
+                        scale: 1,
+                        x: 0,
+                      }}
+                      exit={{
+                        filter: 'blur(6px)',
+                        opacity: 0,
+                        scale: 0,
+                        x: -60,
+                      }}
                       transition={{
-                        duration: 0.3,
+                        duration: 0.6,
                         type: 'spring',
                         bounce: 0,
                         delay: 0,
@@ -114,8 +135,13 @@ export default function SharedLayout() {
           <div className='pointer-events-none absolute right-0 top-0 h-full w-10'>
             <div className='pointer-events-none absolute right-0 top-0 h-full w-24 rounded-full bg-gradient-to-r from-transparent to-[#08090A] to-60%'></div>
           </div>
-          <span className='absolute right-2 top-1/2 -translate-y-1/2 transform'>
-            <img src='/chat/send.svg' alt='send' />
+          <span className='absolute right-1 top-1/2 -translate-y-1/2 transform'>
+            <motion.img
+              whileTap={{ scale: 0.95 }}
+              src='/chat/send.svg'
+              alt='send'
+              className='cursor-pointer p-1'
+            />
           </span>
         </motion.div>
       </div>
