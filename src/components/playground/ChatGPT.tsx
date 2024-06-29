fd975bcf678233b3a9ed24e3725ed13e11ec433d
Breno Lasserre <lasserrebreno@gmail.com>
2024-06-29 18:04:07 -0300

    ChatGPT Fix animation

--- src/components/playground/ChatGPT.tsx
+++ src/components/playground/ChatGPT.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-
 import { motion, AnimatePresence, MotionConfig } from 'framer-motion';
 
 export default function SharedLayout() {
@@ -23,8 +22,8 @@ export default function SharedLayout() {
               width: open ? '6em' : '2.5em',
             }}
           >
-            {open ? (
-              <AnimatePresence initial={false}>
+            <AnimatePresence initial={false} mode='popLayout'>
+              {open ? (
                 <motion.div
                   className='flex items-center justify-center gap-2'
                   style={{ height: '40px' }}
@@ -34,14 +33,18 @@ export default function SharedLayout() {
                     onClick={() => setOpen(!open)}
                   >
                     <motion.img
-                      initial={{ scale: 0 }}
-                      animate={{ scale: 1 }}
-                      exit={{ scale: 0 }}
+                      initial={{ scale: 0, opacity: 0 }}
+                      animate={{ scale: 1, opacity: 1 }}
+                      exit={{
+                        scale: 0,
+                        opacity: 0,
+                        transition: { duration: 0.3, delay: 0 },
+                      }}
                       transition={{
                         duration: 0.6,
                         type: 'spring',
-                        bounce: 0,
-                        delay: 0.4,
+                        bounce: 0.3,
+                        delay: 0.3,
                       }}
                       src='/chat/clip.svg'
                       alt='clip'
@@ -54,11 +57,16 @@ export default function SharedLayout() {
                     <motion.img
                       initial={{ filter: 'blur(4px)', x: -30, scale: 0 }}
                       animate={{ filter: 'blur(0px)', x: 0, scale: 1 }}
-                      exit={{ filter: 'blur(4px)', x: -30, scale: 0 }}
+                      exit={{
+                        filter: 'blur(4px)',
+                        x: -30,
+                        scale: 0,
+                        transition: { duration: 0.3 },
+                      }}
                       transition={{
                         duration: 0.6,
                         type: 'spring',
-                        bounce: 0,
+                        bounce: 0.3,
                         delay: 0.2,
                       }}
                       src='/chat/folder.svg'
@@ -88,11 +96,12 @@ export default function SharedLayout() {
                         opacity: 0,
                         scale: 0,
                         x: -60,
+                        transition: { duration: 0.3 },
                       }}
                       transition={{
                         duration: 0.6,
                         type: 'spring',
-                        bounce: 0,
+                        bounce: 0.3,
                         delay: 0,
                       }}
                       src='/chat/img.svg'
@@ -100,27 +109,34 @@ export default function SharedLayout() {
                     />
                   </motion.button>
                 </motion.div>
-              </AnimatePresence>
-            ) : (
-              <AnimatePresence initial={false}>
-                <motion.button
-                  whileTap={{ scale: 0.95 }}
-                  style={{ height: '40px' }}
-                  onClick={() => setOpen(!open)}
-                  className='w-10 rounded-full bg-[#201F20] p-2'
-                  initial={{ scale: 0, opacity: 0, x: 60 }}
-                  animate={{ scale: 1, opacity: 1, x: 0 }}
-                  exit={{ scale: 0, opacity: 0, x: 60 }}
+              ) : (
+                <motion.div
+                  key='add-button-wrapper'
+                  initial={{ filter: 'blur(6px)', scale: 0, opacity: 0, x: 60 }}
+                  animate={{ filter: 'blur(0px)', scale: 1, opacity: 1, x: 0 }}
+                  exit={{ filter: 'blur(6px)', opacity: 0, scale: 0, x: 60 }}
+                  transition={{
+                    duration: 0.6,
+                    type: 'spring',
+                    bounce: 0.3,
+                  }}
                 >
-                  <motion.img src='/chat/add.svg' alt='add' />
-                </motion.button>
-              </AnimatePresence>
-            )}
+                  <motion.button
+                    whileTap={{ scale: 0.95 }}
+                    style={{ height: '40px' }}
+                    onClick={() => setOpen(!open)}
+                    className='w-10 rounded-full bg-[#201F20] p-2'
+                  >
+                    <motion.img src='/chat/add.svg' alt='add' />
+                  </motion.button>
+                </motion.div>
+              )}
+            </AnimatePresence>
           </motion.div>
         </motion.div>
 
         <motion.div
-          className='relative ml-auto flex items-center  rounded-full border-2 border-[#201F20] text-[#787878]'
+          className='relative ml-auto flex items-center rounded-full border-2 border-[#201F20] text-[#787878]'
           style={{ height: '40px' }}
           initial={{ width: '100%' }}
           animate={{
